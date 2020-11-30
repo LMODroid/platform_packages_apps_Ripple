f0224ce9ef1f9709bc757b86102a4b666f7976c5
Michael Bestas <mkbestas@gmail.com>
2020-12-01 01:29:11 +0200

    Fix QS tile code style and gradle build
    Change-Id: I947fbd5fbb41a46973b3e6e3f6e37d72f55fbb1c

--- app/src/main/java/info/guardianproject/ripple/PanicTileService.java
+++ app/src/main/java/info/guardianproject/ripple/PanicTileService.java
@@ -1,17 +1,21 @@
 package info.guardianproject.ripple;
 
 import android.content.Intent;
+import android.os.Build;
 import android.service.quicksettings.TileService;
 
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class PanicTileService extends TileService {
-	public static final String TAG = "PanicTileService";
+    public static final String TAG = "PanicTileService";
 
-	@Override
-	public void onClick () {
-		super.onClick();
+    @Override
+    public void onClick() {
+        super.onClick();
 
-		Intent intent = new Intent(this, CountDownActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		startActivityAndCollapse(intent);
-	}
+        Intent intent = new Intent(this, CountDownActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityAndCollapse(intent);
+    }
 }
